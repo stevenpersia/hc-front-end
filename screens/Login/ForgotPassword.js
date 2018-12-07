dd3427fcaa94599b14eead1c8ea8442637c8cbdd
Steven Persia <persia.steven8@gmail.com>
2018-12-07 18:43:07 +0100

    Create Forgot Password screen

--- screens/Login/ForgotPassword.js
+++ screens/Login/ForgotPassword.js
@@ -1,13 +1,72 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+	TouchableOpacity,
+	Text,
+	View,
+	TextInput,
+	KeyboardAvoidingView
+} from 'react-native';
 import styles from '../../Styles';
 
 class ForgotPassword extends React.Component {
+	state = {
+		phoneNumber: '',
+		error: false
+	};
+
+	// Add input changes to state
+	handleChange = (key, value) => {
+		this.setState({ [key]: value });
+	};
+
+	askPassword = () => {};
+
 	render() {
 		return (
-			<View style={styles.container}>
-				<Text>This is the ForgotPassword screen</Text>
-			</View>
+			<KeyboardAvoidingView
+				style={[styles.container, { justifyContent: 'center' }]}
+				behavior="padding"
+				enabled
+			>
+				<Text style={styles.h4}>Mot de passe oublié ?</Text>
+				<Text
+					style={[
+						styles.text,
+						styles.paddingV10,
+						styles.w100,
+						styles.textCenter
+					]}
+				>
+					Veuillez entrer votre numéro de téléphone pour recevoir votre mot de
+					passe.
+				</Text>
+				<TextInput
+					style={styles.input}
+					placeholder="Téléphone"
+					keyboardType="numeric"
+					maxLength={10}
+					onChangeText={value => {
+						this.handleChange('phoneNumber', value);
+					}}
+					value={this.state.phoneNumber}
+				/>
+				<Text style={[styles.error]}>
+					{this.state.error === true ? 'Téléphone invalide' : ''}
+				</Text>
+				<TouchableOpacity
+					onPress={this.askPassword}
+					style={[
+						styles.button,
+						styles.primaryButtonColor,
+						styles.marginV10,
+						styles.w100
+					]}
+				>
+					<Text style={[styles.textCenter, styles.textWhite]}>
+						Recevoir le mot de passe
+					</Text>
+				</TouchableOpacity>
+			</KeyboardAvoidingView>
 		);
 	}
 }
