24b872d7d793be60fd1b89288dd3235dd50ba558
gashmal <tovonandroy@gmail.com>
2018-12-07 15:04:02 +0100

    commit before rebase

--- screens/Challenges/ChallengesList.js
+++ screens/Challenges/ChallengesList.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import { StyleSheet, Text, View, FlatList, StatusBar } from "react-native";
 import styles from "../../Styles";
@@ -49,3 +50,56 @@ const customStyles = StyleSheet.create({
 */
 
 export default ChallengesList;
+=======
+import React from "react";
+import { StyleSheet, Text, View, FlatList, StatusBar } from "react-native";
+import styles from "../../Styles";
+import ChallengeCard from "../../components/ChallengeCard";
+import axios from "axios";
+
+class ChallengesList extends React.Component {
+	_keyExtractor = (item, index) => item._id;
+
+	state = {};
+
+	getChallenges() {
+		axios
+			.get(
+				"https://human-challenge-back-end.herokuapp.com/api/challenge?distance=300000000"
+			)
+			.then(response => {
+				this.setState(response.data, () => {
+					// console.log(this.state);
+				});
+			});
+	}
+
+	componentDidMount() {
+		this.getChallenges();
+	}
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<FlatList
+					data={this.state.Challenges}
+					keyExtractor={this._keyExtractor}
+					renderItem={({ item }) => (
+						<View style={{ marginBottom: 1 }}>
+							<ChallengeCard id={item._id} challenge={item} />
+						</View>
+					)}
+				/>
+			</View>
+		);
+	}
+}
+
+/*
+const customStyles = StyleSheet.create({
+	customCSS: {}
+});
+*/
+
+export default ChallengesList;
+>>>>>>> commit before rebase
